ae25da78bcea7270842738b4aff0270e9ed87c94
heath <heath@RACCOONTRASHCAN>
2024-04-02 18:43:28 +0200

    Add project files.

---
+++ MyWebApplication2/Controllers/MyWorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApplication2.Controllers
+{
+    public class MyWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyWebApplication2/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApplication2.Models;
+
+namespace MyWebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public Table_1 userTB = new Table_1();
+
+        [HttpPost]
+        public ActionResult About(Table_1 Users)
+        {
+            var result = userTB.insert_User(Users);
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+
+        public ActionResult About()
+        {
+            return View(userTB);
+        }
+    }
+}
